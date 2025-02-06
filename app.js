960e84bdafefda8c7d8130f24bef8c649d10b60f
vinayBagde <vinaybagde0@gmail.com>
2025-02-06 13:35:10 +0530

    basic setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+
+const MONGO_URL = "mongodb://127.0.0.1:12707/wonderlust";
+main()
+.then(() => {
+    console.log("connection successfull");
+})
+.catch((err) => {
+    console.log(err);
+})
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => {
+    res.send("Hi! i'm root");
+})
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+})
