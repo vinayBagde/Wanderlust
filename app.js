c95494a177a003f6dc656e88c662580a5ac1a09d
vinayBagde <vinaybagde0@gmail.com>
2025-02-07 11:31:59 +0530

    listing models

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
-
-const MONGO_URL = "mongodb://127.0.0.1:12707/wonderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
 main()
-.then(() => {
+  .then(() => {
     console.log("connection successfull");
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
 app.get("/", (req, res) => {
-    res.send("Hi! i'm root");
-})
+  res.send("Hi! i'm root");
+});
+
+app.get("/testListing", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My new villa",
+    description: "By the beach",
+    price: 1200,
+    location: "Calangute, Goa",
+    country: "India",
+  });
+
+  await sampleListing.save();
+  console.log("sample was saved");
+  res.send("successful testing");
+});
 
 app.listen(8080, () => {
-    console.log("server is listening to port 8080");
-})
+  console.log("server is listening to port 8080");
+});
---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG91c2V8ZW58MHx8MHx8fDA%3D",
+    set: (v) =>
+      v === ""
+        ? "https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG91c2V8ZW58MHx8MHx8fDA%3D"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
