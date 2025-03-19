22a130f9843f4a58e6b71050e6820fb6e2224c50
vinayBagde <vinaybagde0@gmail.com>
2025-03-19 06:26:03 +0530

    correction

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const validateListing = (req, res, next) => {
    let {error} = listingSchema.validate(req.body);
    
    if (error) {
-    let errMsg = error.map((el) => el.message).join(",");
+    let errMsg = error.details.map((el) => el.message).join(",");
      throw new ExpressError(400, errMsg);
    } else {
     next();
