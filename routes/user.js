17d6aa0aef58285a6eb7b608a1aefb520cdae038
vinayBagde <vinaybagde0@gmail.com>
2025-06-28 14:14:04 +0530

    Add login page with Passport-based authentication

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
@@ -14,13 +15,29 @@ router.post(
       let { username, email, password } = req.body;
       const newUser = new User({ email, username });
       const registeredUser = await User.register(newUser, password);
-      req.flash("success", "welcome to wonderlust!");
+      req.flash("success", "welcome to Wanderlust!");
       res.redirect("/listings");
     } catch (err) {
       req.flash("error", err.message);
       res.redirect("/signup");
     }
   })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "welcome back to Wanderlust");
+    res.redirect("/listings");
+  }
 );
 
 module.exports = router;
