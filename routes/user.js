6cfba1dd6647825f81a23ca3f3be0c955d3333a4
vinayBagde <vinaybagde0@gmail.com>
2025-06-28 13:57:30 +0530

    Set up backend route for user registration

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
+
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+      req.flash("success", "welcome to wonderlust!");
+      res.redirect("/listings");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/signup");
+    }
+  })
+);
 
 module.exports = router;
