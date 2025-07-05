c2daf961d1801754975b65cc39ce680983dbf0c0
vinayBagde <vinaybagde0@gmail.com>
2025-07-05 23:19:50 +0530

    Add author reference in review schema and protect review form for authenticated users

--- routes/review.js
+++ routes/review.js
@@ -3,16 +3,19 @@ const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const { validateReview } = require("../middleware.js");
+const { validateReview, isLoggedIn } = require("../middleware.js");
 
 //Reviews
 // Post Review Route
 router.post(
   "/",
+  isLoggedIn,
   validateReview,
   wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
+    newReview.author = req.user._id;
+    console.log(newReview);
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
