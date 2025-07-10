bdafb3abac4df07f475f09b2d0eb6cd7600c094e
vinayBagde <vinaybagde0@gmail.com>
2025-07-10 15:55:13 +0530

    Configure Cloudinary to store image uploads

--- routes/listing.js
+++ routes/listing.js
@@ -4,7 +4,8 @@ const wrapAsync = require("../utils/wrapAsync");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router
   .route("/")
