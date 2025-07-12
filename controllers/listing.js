571a9d5f1f6cc80f4d295b09ffa88f2cf9641d5e
vinayBagde <vinaybagde0@gmail.com>
2025-07-12 17:54:11 +0530

    Add image preview functionality on edit page

--- controllers/listing.js
+++ controllers/listing.js
@@ -45,7 +45,9 @@ module.exports.renderEditForm = async (req, res) => {
     req.flash("error", "Listing you requested for does not exist!");
     res.redirect("/listings");
   }
-  res.render("listings/edit.ejs", { listing });
+  let originalImageUrl = listing.image.url;
+  originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_250,w_250");
+  res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
