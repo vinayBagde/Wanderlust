2a27e6463ee9e8f95469526f33bbddc22f665f4d
vinayBagde <vinaybagde0@gmail.com>
2026-04-12 12:06:17 +0530

    add sample data in init file

--- init/index.js
+++ init/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const initData = require("./data.js");
+const initData = require("./sampleData.js");
 const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -16,7 +16,10 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({ ...obj, owner: "686790bbe6fe39bbca39899e" }));
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "686790bbe6fe39bbca39899e",
+  }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
